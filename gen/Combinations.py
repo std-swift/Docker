e40d7f9cdf25e85a34b7ec5eb3414b9dec08d5ca
David Skrundz <david@skrundz.ca>
2019-02-18 20:58:32 -0700

    Add git for cloning dependencies

--- gen/Combinations.py
+++ gen/Combinations.py
@@ -78,6 +78,7 @@ kd1 = [
 ]
 
 bd1 = [
+	'git',
 	'clang',
 	'binutils',
 	'libicu-dev',
@@ -90,7 +91,7 @@ dd1 = [
 ]
 
 Combinations = [
-	Combination(OS.Ubuntu1404, Swift.Swift500, [], kd1 + ['perl'], bd1, dd1),
+	Combination(OS.Ubuntu1404, Swift.Swift500, [], kd1, bd1, dd1),
 	Combination(OS.Ubuntu1604, Swift.Swift500, [], kd1, bd1, dd1),
 	Combination(OS.Ubuntu1804, Swift.Swift500, [], kd1 + ['gnupg2'], bd1 + ['libbsd-dev'], dd1),
 ]
